8d5feeac1b0228ecfc9aac0f34d5ee9948f8b55f
majeretft <majeretft@mail.ru>
2016-07-18 11:59:29 +0300

    Fix bug: Bundles 404 error on release. Fix bug: Footer layout whitespace if yandex share fails.

--- Hozpm/App_Start/BundleConfig.cs
+++ Hozpm/App_Start/BundleConfig.cs
@@ -7,33 +7,33 @@ namespace Hozpm
 		public static void Register(BundleCollection bundles)
 		{
 			//frameworks css
-			bundles.Add(new StyleBundle("~/css/bootstrap.css", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css")
+			bundles.Add(new StyleBundle("~/css/bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css")
 				.Include("~/Content/bootstrap.css"));
 
 			//frameworks js
-			bundles.Add(new ScriptBundle("~/js/jquery.js", "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js")
+			bundles.Add(new ScriptBundle("~/js/jquery", "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js")
 				.Include("~/Scripts/jquery-{version}.js"));
-			bundles.Add(new ScriptBundle("~/js/bootstrap.js", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js")
+			bundles.Add(new ScriptBundle("~/js/bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js")
 				.Include("~/Scripts/bootstrap.js"));
-			bundles.Add(new ScriptBundle("~/js/knockout.js")
+			bundles.Add(new ScriptBundle("~/js/knockout", "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min.js")
 				.Include("~/Scripts/knockout-{version}.js"));
-			bundles.Add(new ScriptBundle("~/js/require.js")
+			bundles.Add(new ScriptBundle("~/js/require", "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.2.0/require.min.js")
 				.Include("~/Scripts/require.js"));
-			bundles.Add(new ScriptBundle("~/js/sammy.js")
+			bundles.Add(new ScriptBundle("~/js/sammy", "https://cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.6/sammy.min.js")
 				.Include("~/Scripts/sammy-{version}.js"));
 
 			//website css
-			bundles.Add(new StyleBundle("~/css/site.css")
+			bundles.Add(new StyleBundle("~/css/site")
 				.Include("~/css/site.css"));
-			bundles.Add(new StyleBundle("~/css/user.css")
+			bundles.Add(new StyleBundle("~/css/user")
 				.Include("~/css/user.css"));
-			bundles.Add(new StyleBundle("~/css/ie.css")
+			bundles.Add(new StyleBundle("~/css/ie")
 				.Include("~/css/ie.css"));
 
 			//website js
-			bundles.Add(new ScriptBundle("~/app/app.loader.js")
+			bundles.Add(new ScriptBundle("~/app/loader")
 				.Include("~/app/app.loader.js"));
-			bundles.Add(new ScriptBundle("~/app/app.js")
+			bundles.Add(new ScriptBundle("~/app/app")
 				.Include("~/app/app.js"));
 		}
 	}
