0d22d5359f4a03084fa52026a443de8772868167
majeretft <majeretft@mail.ru>
2016-11-07 14:12:28 +0300

    Added error logging.

--- Hozpm/Global.asax.cs
+++ Hozpm/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Net;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Hozpm.Logic;
 
 namespace Hozpm
 {
@@ -30,7 +32,53 @@ namespace Hozpm
 
 		protected void Application_Error(object sender, EventArgs e)
 		{
-			// nothing
+			var exc = Server.GetLastError();
+
+			try
+			{
+				ExceptionUtility.LogException(exc);
+			}
+			catch(Exception error)
+			{
+				Response.Clear();
+				Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+				Response.Write("<h1>Error handler exception</h1>");
+				Response.Write("<hr/>");
+
+				Response.Write("<h2>Logger exception details</h2>");
+				Response.Write($"<p>Type: {error.GetType()}</p>");
+				Response.Write($"<p>Message: {error.Message}</p>");
+				Response.Write($"<p>Source: {error.Source}</p>");
+				Response.Write("<p>Stack trace:</p>");
+				if (error.StackTrace != null)
+					Response.Write($"<pre>{error.StackTrace}</pre>");
+
+				Response.Write("<hr/>");
+
+				Response.Write("<h2>Original exception</h2>");
+				Response.Write($"<p>Type: {exc.GetType()}</p>");
+				Response.Write($"<p>Message: {exc.Message}</p>");
+				Response.Write($"<p>Source: {exc.Source}</p>");
+				Response.Write("<p>Stack trace:</p>");
+				if (error.StackTrace != null)
+					Response.Write($"<pre>{exc.StackTrace}</pre>");
+
+				if (exc.InnerException != null)
+				{
+					Response.Write("<hr/>");
+
+					Response.Write("<h2>Original inner exception</h2>");
+					Response.Write($"<p>Type: {exc.InnerException.GetType()}</p>");
+					Response.Write($"<p>Message: {exc.InnerException.Message}</p>");
+					Response.Write($"<p>Source: {exc.InnerException.Source}</p>");
+					Response.Write("<p>Stack trace:</p>");
+					if (error.StackTrace != null)
+						Response.Write($"<pre>{exc.InnerException.StackTrace}</pre>");
+				}
+
+				Server.ClearError();
+			}
 		}
 
 		protected void Session_End(object sender, EventArgs e)
---
+++ Hozpm/Logic/ExceptionUtility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+// Create our own utility for exceptions 
+namespace Hozpm.Logic
+{
+	public static class ExceptionUtility
+	{
+		public const string LogPath = "App_Data/Application_Error.txt";
+
+		// Log an Exception 
+		public static void LogException(Exception exc)
+		{
+			// Include enterprise logic for logging exceptions 
+			// Get the absolute path to the log file 
+			var logFile = HttpContext.Current.Server.MapPath(LogPath);
+
+			// Open the log file for append and write the log
+			var sw = new StreamWriter(logFile, true);
+			sw.WriteLine("********** {0} **********", DateTime.Now);
+
+			sw.WriteLine($"Exception Type: {exc.GetType()}");
+			sw.WriteLine($"Message: {exc.Message}");
+			sw.WriteLine($"Source: {exc.Source}");
+			if (exc.StackTrace != null)
+			{
+				sw.WriteLine("Stack Trace: ");
+				sw.WriteLine(exc.StackTrace);
+				sw.WriteLine();
+			}
+
+			if (exc.InnerException != null)
+			{
+				sw.WriteLine($"Inner Exception Type: {exc.InnerException.GetType()}");
+				sw.WriteLine($"Message: {exc.InnerException.Message}");
+				sw.WriteLine($"Source: {exc.InnerException.Source}");
+				if (exc.InnerException.StackTrace != null)
+				{
+					sw.WriteLine("Inner Stack Trace: ");
+					sw.WriteLine(exc.InnerException.StackTrace);
+					sw.WriteLine();
+				}
+			}
+			
+			sw.Close();
+		}
+	}
+}
